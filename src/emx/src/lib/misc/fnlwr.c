95d3ac3d5728832b3434230e2cbec4523f986960
Dmitriy Kuminov <coding@dmik.org>
2019-07-09 20:13:57 +0400

    Make _fnlwr/_fnlwr2 work for path rewrites (/@unixroot and such).
    It would incorrectly detect FS type for such paths if the current
    drive didn't match the original path drive (e.g. was a CDROM). This
    would in turn lead to really fancy bugs like python not being able
    to find UserDict.py and other mixed case modules when started from
    a directory of a CDROM drive.
    
    This commit also removes unconditional lowering of the drive letter
    which happened in readdir (which uses _fnlwr) even on FSes supporting
    mixed case.
    
    Fixes #39.

--- src/emx/src/lib/misc/fnlwr.c
+++ src/emx/src/lib/misc/fnlwr.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <InnoTekLIBC/pathrewrite.h>
 
 static char cache_curdrive = 0;
 static char cache_filesys[26] =
@@ -12,60 +13,79 @@ static char cache_filesys[26] =
 
 void _fnlwr2 (char *name, const char *base)
 {
-  char c, dr[3], fs[16];
-  int i;
+    char c, dr[3], fs[16];
+    int i;
 
-  /* Do not translate UNC paths to lower case. */
+    /* Do not translate UNC paths to lower case. */
 
-  if (DIRSEP_P (base[0]) && DIRSEP_P (base[1]) && !DIRSEP_P (base[2]))
-    return;
-
-  c = _fngetdrive (base);
-  if (c == 0)
-    {
-      if (cache_curdrive == 0)
-        cache_curdrive = _getdrive ();
-      c = cache_curdrive;
-    }
-  else if (name == base)
-    name[0] = (char)(c - 'A' + 'a');
-  i = c - 'A';
-  if (cache_filesys[i] == 0)
-    {
-      dr[0] = c;
-      dr[1] = ':';
-      dr[2] = 0;
-      if (_filesys (dr, fs, sizeof (fs)) != 0)
+    if (DIRSEP_P (base[0]) && DIRSEP_P (base[1]) && !DIRSEP_P (base[2]))
         return;
-      if (strcmp (fs, "FAT") == 0 || strcmp (fs, "CDFS") == 0)
-        cache_filesys[i] = 'u';     /* upper-casing */
-      else
-        cache_filesys[i] = 'p';     /* case-preserving, perhaps */
+
+    c = _fngetdrive (base);
+    if (c == 0)
+    {
+        /*
+         * Rewrite base to get its actual drive letter (and refuse to
+         * perform translation if the path needs rewriting but there is
+         * a fatal failure like no memory or so).
+         */
+        int cch = __libc_PathRewrite (base, NULL, 0);
+        if (cch > 0)
+        {
+            char *pszRewritten = alloca (cch);
+            if (!pszRewritten)
+                return;
+            cch = __libc_PathRewrite (base, pszRewritten, cch);
+            if (cch > 0)
+                c = _fngetdrive (pszRewritten);
+            /* else happens when someone changes the rules between the two calls. */
+            else if (cch < 0)
+                return;
+        }
+        if (c == 0)
+        {
+            if (cache_curdrive == 0)
+                cache_curdrive = _getdrive ();
+            c = cache_curdrive;
+        }
     }
-  if (cache_filesys[i] == 'u')
-    strlwr (name);
+    i = c - 'A';
+    if (cache_filesys[i] == 0)
+    {
+        dr[0] = c;
+        dr[1] = ':';
+        dr[2] = 0;
+        if (_filesys (dr, fs, sizeof (fs)) != 0)
+            return;
+        if (strcmp (fs, "FAT") == 0 || strcmp (fs, "CDFS") == 0)
+            cache_filesys[i] = 'u';     /* upper-casing */
+        else
+            cache_filesys[i] = 'p';     /* case-preserving, perhaps */
+    }
+    if (cache_filesys[i] == 'u')
+        strlwr (name);
 }
 
 
 void _fnlwr (char *name)
 {
-  _fnlwr2 (name, name);
+    _fnlwr2 (name, name);
 }
 
 
 void _rfnlwr (void)
 {
-  int i;
+    int i;
 
-  for (i = 0; i < 26; ++i)
-    cache_filesys[i] = 0;
-  cache_curdrive = 0;
+    for (i = 0; i < 26; ++i)
+        cache_filesys[i] = 0;
+    cache_curdrive = 0;
 }
 
 
 void _sfnlwr (const char *name)
 {
-  cache_curdrive = _fngetdrive (name);
-  if (cache_curdrive == 0)
-    cache_curdrive = _getdrive ();
+    cache_curdrive = _fngetdrive (name);
+    if (cache_curdrive == 0)
+        cache_curdrive = _getdrive ();
 }
