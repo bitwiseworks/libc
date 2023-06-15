3581cfabdc0c955d72f621c6ce03c7a293b8d245
Dmitriy Kuminov <coding@dmik.org>
2023-06-16 00:11:29 +0300

    errno: Make EWOULDBLOCK, ETXTBSY and others available in POSIX mode.
    These errors as well as some more are part of the POSIX standard for
    quite some time and therefore should be defined even in POSIX mode.
    
    Closes #124.

--- src/emx/include/sys/errno.h
+++ src/emx/include/sys/errno.h
@@ -91,9 +91,7 @@ __END_DECLS
 #define	EMFILE		24		/* Too many open files */
 #define	ENOTTY		25		/* Inappropriate ioctl for device */
 /* bird: old emx #define EDEADLK 26 */
-#ifndef _POSIX_SOURCE
 #define	ETXTBSY		26		/* Text file busy */
-#endif
 #define	EFBIG		27		/* File too large */
 #define	ENOSPC		28		/* No space left on device */
 #define	ESPIPE		29		/* Illegal seek */
@@ -108,24 +106,27 @@ __END_DECLS
 /* non-blocking and interrupt i/o */
 /* bird: old emx #define ENOTEMPTY 35 */
 #define	EAGAIN		35		/* Resource temporarily unavailable */
-#ifndef _POSIX_SOURCE
 #define	EWOULDBLOCK	EAGAIN		/* Operation would block */
 #define	EINPROGRESS	36		/* Operation now in progress */
 /* bird: old emx#define ENOSYS 37 */
 #define	EALREADY	37		/* Operation already in progress */
-
-/* ipc/network software -- argument errors */
 /* bird: old emx #define ENAMETOOLONG 38 */
 #define	ENOTSOCK	38		/* Socket operation on non-socket */
 #define	EDESTADDRREQ	39		/* Destination address required */
+
+/* ipc/network software -- argument errors */
 #define	EMSGSIZE	40		/* Message too long */
 #define	EPROTOTYPE	41		/* Protocol wrong type for socket */
 #define	ENOPROTOOPT	42		/* Protocol not available */
 #define	EPROTONOSUPPORT	43		/* Protocol not supported */
+#ifndef _POSIX_SOURCE
 #define	ESOCKTNOSUPPORT	44		/* Socket type not supported */
+#endif
 #define	EOPNOTSUPP	45		/* Operation not supported */
 #define	ENOTSUP		EOPNOTSUPP	/* Operation not supported */
+#ifndef _POSIX_SOURCE
 #define	EPFNOSUPPORT	46		/* Protocol family not supported */
+#endif
 #define	EAFNOSUPPORT	47		/* Address family not supported by protocol family */
 #define	EADDRINUSE	48		/* Address already in use */
 #define	EADDRNOTAVAIL	49		/* Can't assign requested address */
@@ -139,21 +140,22 @@ __END_DECLS
 #define	ENOBUFS		55		/* No buffer space available */
 #define	EISCONN		56		/* Socket is already connected */
 #define	ENOTCONN	57		/* Socket is not connected */
+#ifndef _POSIX_SOURCE
 #define	ESHUTDOWN	58		/* Can't send after socket shutdown */
 #define	ETOOMANYREFS	59		/* Too many references: can't splice */
+#endif
 #define	ETIMEDOUT	60		/* Operation timed out */
 #define	ECONNREFUSED	61		/* Connection refused */
 
 #define	ELOOP		62		/* Too many levels of symbolic links */
-#endif /* _POSIX_SOURCE */
 /* bird: old emx #define ENOTSOCK 63 */
 #define	ENAMETOOLONG	63		/* File name too long */
 
 /* should be rearranged */
 #ifndef _POSIX_SOURCE
 #define	EHOSTDOWN	64		/* Host is down */
+#endif
 #define	EHOSTUNREACH	65		/* No route to host */
-#endif /* _POSIX_SOURCE */
 /* bird: old emx #define EALREADY 66 */
 #define	ENOTEMPTY	66		/* Directory not empty */
 
@@ -161,17 +163,19 @@ __END_DECLS
 #ifndef _POSIX_SOURCE
 #define	EPROCLIM	67		/* Too many processes */
 #define	EUSERS		68		/* Too many users */
+#endif
 #define	EDQUOT		69		/* Disc quota exceeded */
 
 /* Network File System */
 #define	ESTALE		70		/* Stale NFS file handle */
+#ifndef _POSIX_SOURCE
 #define	EREMOTE		71		/* Too many levels of remote in path */
 #define	EBADRPC		72		/* RPC struct is bad */
 #define	ERPCMISMATCH	73		/* RPC version wrong */
 #define	EPROGUNAVAIL	74		/* RPC prog. not avail */
 #define	EPROGMISMATCH	75		/* Program version wrong */
 #define	EPROCUNAVAIL	76		/* Bad procedure for program */
-#endif /* _POSIX_SOURCE */
+#endif
 
 #define	ENOLCK		77		/* No locks available */
 #define	ENOSYS		78		/* Function not implemented */
@@ -180,18 +184,21 @@ __END_DECLS
 #define	EFTYPE		79		/* Inappropriate file type or format */
 #define	EAUTH		80		/* Authentication error */
 #define	ENEEDAUTH	81		/* Need authenticator */
+#endif
 #define	EIDRM		82		/* Identifier removed */
 #define	ENOMSG		83		/* No message of desired type */
 #define	EOVERFLOW	84		/* Value too large to be stored in data type */
 #define	ECANCELED	85		/* Operation canceled */
 #define	EILSEQ		86		/* Illegal byte sequence */
+#ifndef _POSIX_SOURCE
 #define	ENOATTR		87		/* Attribute not found */
+#endif
 
+#ifndef _POSIX_SOURCE
 #define EDOOFUS		88		/* Programming error */
+#endif
 
 #define	ELAST		88		/* Must be equal largest errno */
-
-#endif /* _POSIX_SOURCE */
 
 #ifdef __USE_EMX
 #define ENEWVER         90              /* mixing with a new version of a struct */
