0259f844a11a389bc18593a8fdb5f8a61f21c06f
Dmitriy Kuminov <coding@dmik.org>
2023-08-30 22:06:41 +0300

    assert.h: Add static_assert macro (C11 standard).
    The code is borrowed from https://github.com/freebsd/freebsd-src/blob/de1aa3dab23c06fec962a14da3e7b4755c5880cf/include/assert.h.
    
    Closes #131.

--- src/emx/include/assert.h
+++ src/emx/include/assert.h
@@ -3,6 +3,13 @@
  * EMX
  */
 
+#include <sys/cdefs.h>
+
+/*
+ * Unlike other ANSI header files, <assert.h> may usefully be included
+ * multiple times, with and without NDEBUG defined.
+ */
+
 #if defined (__cplusplus)
 extern "C" {
 #endif
@@ -15,6 +22,24 @@ extern "C" {
 void _assert (const char *, const char *, unsigned);
 #define assert(exp) ((exp) ? (void)0 : _assert (#exp, __FILE__, __LINE__))
 #endif
+
+#ifndef _ASSERT_H_
+#define _ASSERT_H_
+
+/*
+ * Static assertions.  In principle we could define static_assert for
+ * C++ older than C++11, but this breaks if _Static_assert is
+ * implemented as a macro.
+ *
+ * C++ template parameters may contain commas, even if not enclosed in
+ * parentheses, causing the _Static_assert macro to be invoked with more
+ * than two parameters.
+ */
+#if __ISO_C_VISIBLE >= 2011 && !defined(__cplusplus)
+#define	static_assert	_Static_assert
+#endif
+
+#endif /* !_ASSERT_H_ */
 
 #if defined (__cplusplus)
 }
